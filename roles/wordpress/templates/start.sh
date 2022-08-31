3b09dc84dfaccda3a09e2ccc5084f0fa934f8c5e
NatKW <92329992+NatKW@users.noreply.github.com>
2022-08-31 18:10:07 +0200

    encours

--- roles/wordpress/templates/start.sh
+++ roles/wordpress/templates/start.sh
@@ -16,7 +16,6 @@ if [ ! -f "/var/www/html/index.php" ]; then
     wp config create --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=mariadb --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
     wp core install --url=$DOMAIN_NAME --title=$WORDPRESS_DB_TITLE --admin_user=$WORDPRESS_DB_ADMIN --admin_password=$WORDPRESS_DB_ADMIN_PWD --admin_email=$WORDPRESS_DB_ADMIN_EMAIL --skip-email --allow-root
     wp user create $W_USER $W_EMAIL --role=author --user_pass=$W_PWD --allow-root
-    wp user create paul ddalee@hotmail.com --role=author --user_pass=123123 --allow-root
     wp theme install  twenty sixteen --activate --allow-root
     wp theme update twenty sixteen --allow-root
     wp plugin update --all --allow-root
